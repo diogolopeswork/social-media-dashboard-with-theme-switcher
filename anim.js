0a380c6d2c677eb2c8f821d20824813acf5fba37
Diogo Lopes <deosl98@gmail.com>
2020-12-27 11:29:17 +0000

    commit

---
+++ anim.js
@@ -0,0 +1,75 @@
+document.querySelector('.header-left').style.opacity = 0;
+document.querySelector('.header-right').style.opacity = 0;
+
+document.getElementById('card1').style.opacity = 0;
+document.getElementById('card2').style.opacity = 0;
+document.getElementById('card3').style.opacity = 0;
+document.getElementById('card4').style.opacity = 0;
+
+document.getElementById('overview-title').style.opacity = 0;
+document.getElementById('Ocard1').style.opacity = 0;
+document.getElementById('Ocard2').style.opacity = 0;
+document.getElementById('Ocard3').style.opacity = 0;
+document.getElementById('Ocard4').style.opacity = 0;
+
+document.getElementById('O2card1').style.opacity = 0;
+document.getElementById('O2card2').style.opacity = 0;
+document.getElementById('O2card3').style.opacity = 0;
+document.getElementById('O2card4').style.opacity = 0;
+
+
+setTimeout(() => {
+    document.querySelector('.header-left').style.opacity = 1;
+}, 200);
+
+setTimeout(() => {
+    document.querySelector('.header-right').style.opacity = 1;
+}, 300);
+
+setTimeout(() => {
+    document.getElementById('card1').style.opacity = 1;
+}, 400);
+
+setTimeout(() => {
+    document.getElementById('card2').style.opacity = 1;
+}, 500);
+
+setTimeout(() => {
+    document.getElementById('card3').style.opacity = 1;
+}, 600);
+
+setTimeout(() => {
+    document.getElementById('card4').style.opacity = 1;
+}, 700);
+
+setTimeout(() => {
+    document.getElementById('Ocard1').style.opacity = 1;
+}, 800);
+
+setTimeout(() => {
+    document.getElementById('Ocard2').style.opacity = 1;
+}, 900);
+
+setTimeout(() => {
+    document.getElementById('Ocard3').style.opacity = 1;
+}, 1000);
+
+setTimeout(() => {
+    document.getElementById('Ocard4').style.opacity = 1;
+}, 1100);
+
+setTimeout(() => {
+    document.getElementById('O2card1').style.opacity = 1;
+}, 1100);
+
+setTimeout(() => {
+    document.getElementById('O2card2').style.opacity = 1;
+}, 1200);
+
+setTimeout(() => {
+    document.getElementById('O2card3').style.opacity = 1;
+}, 1300);
+
+setTimeout(() => {
+    document.getElementById('O2card4').style.opacity = 1;
+}, 1400);
